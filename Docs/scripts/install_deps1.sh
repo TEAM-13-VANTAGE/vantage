3e3e88a0dc07d435b896cde9d7c3f65176bf9063
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2024-11-07 23:56:35 -0500

    updated install_deps1.sh

--- Docs/scripts/install_deps1.sh
+++ Docs/scripts/install_deps1.sh
@@ -34,6 +34,7 @@ cd Micro-XRCE-DDS-Gen
 ./gradlew assemble
 echo "export PATH=\$PATH:$PWD/scripts" >> ~/.bashrc
 source ~/.bashrc
+export PATH=\$PATH:$PWD/scripts
 microxrceddsgen -help
 sudo apt-get install python3-dev python3-opencv python3-wxgtk4.0 python3-pip python3-matplotlib python3-lxml python3-pygame
 pip3 install PyYAML mavproxy --user
